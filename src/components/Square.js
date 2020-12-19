5bccde366b845a25afb1e67e74f45d99213de493
MrPboti <panahi.amirreza@gmail.com>
2020-12-19 22:59:38 +0330

    npm start

--- src/components/Square.js
+++ src/components/Square.js
@@ -7,13 +7,14 @@ const Square = props => (
 <Button 
   className={`${props.winnerClass} square`}
   color="secondary"
-  variant="outlined"   
+  variant="outlined"  
   style={{
     borderRadius: 250,
     width: "80px",
     height:"80px",
     fontSize: "40px",
     padding: "18px",
+    border: '2px solid'
         }}
   onClick={props.onClick}>
 
